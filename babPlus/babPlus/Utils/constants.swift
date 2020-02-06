1f1fbea1eb0ba4fde8fae24fa3408ba2677e9339
hongdonghyun <hong4367@gmail.com>
2020-02-06 21:21:21 +0900

    add Constant.swift -> span Variable Detail View Logic modify

--- babPlus/babPlus/Utils/constants.swift
+++ babPlus/babPlus/Utils/constants.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import MapKit
 
 let mapCenterlat = 37.544792611423425//위도
 let mapCenterlon = 127.05570731194194//경도
+let span = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
